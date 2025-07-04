5f58bed502d319191469ef5f624a3ca7996d846e
firoz.nur <firoznur5@gmail.com>
2025-07-05 03:25:50 +0600

    Revert : "Start : RTK Query"

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+import taskReducer from "./features/task/taskSlice";
+import userReducer from "./features/user/userSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+    todo: taskReducer,
+    user: userReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
