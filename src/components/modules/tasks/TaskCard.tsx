85630f76e5f415330c4126708bcdb4d87b7ccc5d
firoz.nur <firoznur5@gmail.com>
2025-07-05 23:57:03 +0600

    Change: Priority Order

--- src/components/modules/tasks/TaskCard.tsx
+++ src/components/modules/tasks/TaskCard.tsx
@@ -29,9 +29,9 @@ export default function TaskCard({ task }: IProps) {
         <div className="flex gap-2 items-center">
           <div
             className={cn("size-3 rounded-full", {
-              "bg-green-500": task.priority === "Low",
-              "bg-yellow-500": task.priority === "Medium",
               "bg-red-500": task.priority === "High",
+              "bg-yellow-500": task.priority === "Medium",
+              "bg-green-500": task.priority === "Low",
             })}></div>
           <h1
             className={cn("text-xl", {
